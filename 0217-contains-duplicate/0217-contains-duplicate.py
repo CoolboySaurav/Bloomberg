5e5d4d3fbd9bf62a100f42ce3056966a568e3017
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-09-25 17:56:31 -0700

    Time: 432 ms (55.93%), Space: 27.4 MB (46.27%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        numSet = set()
+        
+        for num in nums:
+            if num in numSet:
+                return True
+            numSet.add(num)
+        return False
